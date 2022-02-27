a6391e48548b428005131c5d48b33c296b268f6a
richard.dai <richarddai@163.com>
2022-02-27 13:09:48 +0000

    add function load isa:la lb

--- src/start.c
+++ src/start.c
@@ -1,5 +1,14 @@
 #include <printk.h>
+extern char load_isa_la_lb(int index);
+void asm_test(void)
+{
+    printk("run %s\n", __func__);
+    for(int index; index < 14; index++)
+        printk("%c", load_isa_la_lb(index));
+    printk("end %s\n", __func__);
+}
 void start()
 {
+    asm_test();
     printk("start\n");
 }

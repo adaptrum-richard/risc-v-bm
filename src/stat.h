fe0fb11dbff25be67ddcb64664e18bcf7f47def6
richard.dai <richarddai@163.com>
2022-05-11 08:47:33 +0000

    add sysfile.c

--- src/stat.h
+++ src/stat.h
@@ -1,17 +1,8 @@
 #ifndef __STAT_H__
 #define __STAT_H__
-#include "types.h"
 
 #define T_DIR     1   // Directory
 #define T_FILE    2   // File
 #define T_DEVICE  3   // Device
-
-struct stat {
-  int dev;     // File system's disk device
-  uint ino;    // Inode number
-  short type;  // Type of file
-  short nlink; // Number of links to file
-  uint64 size; // Size of file in bytes
-};
 
 #endif
---
+++ src/sysfile.c
@@ -0,0 +1,135 @@
+#include "types.h"
+#include "log.h"
+#include "file.h"
+#include "fs.h"
+#include "stat.h"
+#include "printk.h"
+#include "string.h"
+#include "fcntl.h"
+#include "proc.h"
+#include "string.h"
+
+static struct inode *create(char *path, short type, 
+    short major, short minor)
+{
+    struct inode *ip, *dp;
+    char name[DIRSIZ];
+    
+    /*找到此文件父目录的inode*/
+    if((dp = nameiparent(path, name)) == 0)
+        return 0;
+
+    ilock(dp);
+    /*在父目录中搜索是否存在此文件*/
+    if((ip = dirlookup(dp, name, 0)) != 0){
+        /*如果找到对应的文件则返回文件对应的inode*/
+        iunlockput(dp);
+        ilock(ip);
+        if(type == T_FILE && ip->type == T_FILE){
+            return ip;
+        }
+        /*打开的文件存在，但是是一个目录，出问题了。
+        这里是否应该处理type为目录的情况？
+        */
+        iunlockput(ip);
+        return 0;
+    }
+    /*到这里说明，没有找到要找的文件，则分配一个inode*/
+    if((ip = ialloc(dp->dev, type)) == 0)
+        panic("create： ialloc\n");
+    
+    /*初始化inode*/
+    ilock(ip);
+    ip->major = major;
+    ip->minor = minor;
+    ip->nlink = 1;
+    iupdate(ip);
+    
+    /*处理创建目录情况*/
+    if(type == T_DIR){
+        /*父目录的nlink加1*/
+        dp->nlink++;
+        iupdate(dp);
+        /*创建此目录下的dots*/
+        if(dirlink(ip, ".", ip->inum) < 0 || dirlink(ip, "..", dp->inum) < 0){
+            panic("create dots\n");
+        }
+    }
+
+    /*创建文件或目录*/
+    if(dirlink(dp, name, ip->inum) < 0)
+        panic("create: dirlink\n");
+    
+    iunlockput(dp);
+    return ip;
+}
+
+static int fdalloc(struct file *f)
+{
+    int fd;
+    struct task_struct *p = current;
+    for(fd = 0; fd < NOFILE; fd++){
+        if(p->ofile[fd] == 0){
+            p->ofile[fd] = f;
+            return fd;
+        }
+    }
+    return -1;
+}
+
+uint64 sys_open(const char *pathname, int omode)
+{
+    char path[MAXPATH] = {0};
+    int fd;
+    struct file *f;
+    struct inode *ip;
+
+    if(!pathname || strlen(pathname) > MAXPATH)
+        return -1;
+    
+    strncpy(path, pathname, MAXPATH);
+
+    log_begin_op();
+    
+    if(omode & O_CREATE){
+        ip = create(path, T_FILE, 0, 0);
+        if(ip == 0){
+            log_end_op();
+            return -1;
+        }
+    } else {
+        if((ip = namei(path)) == 0){
+            log_end_op();
+            return -1;
+        }
+        
+        ilock(ip);
+        if(ip->type == T_DIR && omode != O_RDONLY){
+            iunlockput(ip);
+            log_end_op();
+            return -1;
+        }
+    }
+
+    if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
+        if(f)
+            fileclose(f);
+        iunlockput(ip);
+        log_end_op();
+        return -1;
+    }
+
+    f->type = FD_INODE;
+    f->off = 0;
+
+    f->ip = ip;
+    f->readable = !(omode & O_WRONLY);
+    f->writable = (omode & O_WRONLY) || (omode & O_RDWR);
+
+    if((omode & O_TRUNC) && ip->type == T_FILE){
+        itrunc(ip);
+    }
+    iunlockput(ip);
+    log_end_op();
+    return 0;
+}

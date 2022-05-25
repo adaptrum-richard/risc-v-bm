104abafd7eb3585d980a95616fbb10ff04d96845
richard.dai <richarddai@163.com>
2022-05-25 14:19:55 +0000

    bitops.h: add ffs

--- src/biops.h
+++ src/biops.h
@@ -4,7 +4,7 @@
 #define DIV_ROUND_UP(n, d) (((n) + (d)-1) / (d))
 
 #define BITS_PER_BYTE 8
-#define BITS_PER_LONG (sizeof(unsigned long) * BITS_PER_BYTE)
+#define BITS_PER_LONG (8 * BITS_PER_BYTE)
 #define BITS_TO_LONGS(nr) DIV_ROUND_UP(nr, BITS_PER_LONG)
 #define BIT_MASK(nr) (1UL << ((nr) % BITS_PER_LONG))
 #define BIT_WORD(nr) ((nr) / BITS_PER_LONG)
@@ -135,4 +135,116 @@ static inline int find_next_bit(unsigned long *p, unsigned long size,
     for ((pos) = find_next_bit((addr), (size), 0); \
          (pos) < (size);                           \
          (pos) = find_next_bit((addr), (size), (pos) + 1))
+
+
+static inline int ffs(unsigned long x)
+{
+    int r = 1;
+
+    if (!x)
+    return 0;
+#if BITS_PER_LONG == 64
+    if ((x & 0xffffffff) == 0) {
+        r += 32;
+        x >>= 32;
+    }
+#endif
+    if (!(x & 0xffff)) {
+        x >>= 16;
+        r += 16;
+    }
+    if (!(x & 0xff)) {
+        x >>= 8;
+        r += 8;
+    }
+    if (!(x & 0xf)) {
+        x >>= 4;
+        r += 4;
+    }
+    if (!(x & 3)) {
+        x >>= 2;
+        r += 2;
+    }
+    if (!(x & 1)) {
+        x >>= 1;
+        r += 1;
+    }
+    return r;
+}
+
+static inline unsigned long __ffs(unsigned long word)
+{
+    int num = 0;
+
+#if BITS_PER_LONG == 64
+    if ((word & 0xffffffff) == 0) {
+        num += 32;
+        word >>= 32;
+    }
+#endif
+    if ((word & 0xffff) == 0) {
+        num += 16;
+        word >>= 16;
+    }
+    if ((word & 0xff) == 0) {
+        num += 8;
+        word >>= 8;
+    }
+    if ((word & 0xf) == 0) {
+        num += 4;
+        word >>= 4;
+    }
+    if ((word & 0x3) == 0) {
+        num += 2;
+        word >>= 2;
+    }
+    if ((word & 0x1) == 0)
+        num += 1;
+    return num;
+}
+
+/**
+ * __fls - find last (most-significant) set bit in a long word
+ * @word: the word to search
+ *
+ * Undefined if no set bit exists, so code should check against 0 first.
+ */
+static unsigned long __fls(unsigned long word)
+{
+    int num = BITS_PER_LONG - 1;
+
+#if BITS_PER_LONG == 64
+    if (!(word & (~0ul << 32))) {
+        num -= 32;
+        word <<= 32;
+    }
+#endif
+    if (!(word & (~0ul << (BITS_PER_LONG-16)))) {
+        num -= 16;
+        word <<= 16;
+    }
+    if (!(word & (~0ul << (BITS_PER_LONG-8)))) {
+        num -= 8;
+        word <<= 8;
+    }
+    if (!(word & (~0ul << (BITS_PER_LONG-4)))) {
+        num -= 4;
+        word <<= 4;
+    }
+    if (!(word & (~0ul << (BITS_PER_LONG-2)))) {
+        num -= 2;
+        word <<= 2;
+    }
+    if (!(word & (~0ul << (BITS_PER_LONG-1))))
+        num -= 1;
+    return num;
+ }
+ 
+ static inline unsigned long fls64(unsigned long x)
+ {
+    if (x == 0)
+        return 0;
+    return __fls(x) + 1;
+ }
+
 #endif

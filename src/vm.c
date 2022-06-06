96048e43bbd62d33eb385a845a1a5c41a7d9ce4c
richard.dai <richarddai@163.com>
2022-06-06 08:43:36 +0000

    add uvmunmap

--- src/vm.c
+++ src/vm.c
@@ -43,6 +43,32 @@ pte_t *walk(pagetable_t pagetable, uint64 va, int alloc)
     return &pagetable[PX(0, va)];
 }
 
+// Remove npages of mappings starting from va. va must be
+// page-aligned. The mappings must exist.
+// Optionally free the physical memory.
+void uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
+{
+    uint64 a;
+    pte_t *pte;
+
+    if((va % PGSIZE) != 0)
+        panic("uvmunmap: not aligned");
+
+    for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
+        if((pte = walk(pagetable, a, 0)) == 0)
+            panic("uvmunmap: walk");
+        if((*pte & PTE_V) == 0)
+            panic("uvmunmap: not mapped");
+        if(PTE_FLAGS(*pte) == PTE_V)
+            panic("uvmunmap: not a leaf");
+        if(do_free){
+            uint64 pa = PTE2PA(*pte);
+            free_page((unsigned long)pa);
+        }
+        *pte = 0;
+    }
+}
+
 int mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm)
 {
     uint64 a, last;
--- src/vm.h
+++ src/vm.h
@@ -9,4 +9,5 @@ int uvminit(pagetable_t pagetable, uchar *src, uint size);
 pagetable_t get_kpgtbl();
 pagetable_t copy_kernel_tbl(void);
 int mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm);
+void uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free);
 #endif

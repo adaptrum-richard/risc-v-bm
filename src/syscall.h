c6e2caaea40af68ff134c665f182beaa96e3e333
richard.dai <richarddai@163.com>
2022-06-08 01:59:33 +0000

    add some empty system call functions

--- src/syscall.h
+++ src/syscall.h
@@ -1,7 +1,32 @@
 #ifndef __SYSCALL_H__
 #define __SYSCALL_H__
 
-#define __NR_syscalls 5
+// System call numbers
+#define SYS_printf  0
+#define SYS_fork    1
+#define SYS_exit    2
+#define SYS_wait    3
+#define SYS_pipe    4
+#define SYS_read    5
+#define SYS_kill    6
+#define SYS_exec    7
+#define SYS_fstat   8
+#define SYS_chdir   9
+#define SYS_dup    10
+#define SYS_getpid 11
+#define SYS_brk    12
+#define SYS_sleep  13
+#define SYS_uptime 14
+#define SYS_open   15
+#define SYS_write  16
+#define SYS_mknod  17
+#define SYS_unlink 18
+#define SYS_link   19
+#define SYS_mkdir  20
+#define SYS_close  21
+#define SYS_free   22
+#define __NR_syscalls 24
+
 extern void * const sys_call_table[];
 
 #endif
--- src/syscall_table.c
+++ src/syscall_table.c
@@ -31,10 +31,124 @@ unsigned long sys_free(void *addr)
     return _free((unsigned long)addr);
 }
 
+unsigned long sys_fork(void)
+{
+    return 0;
+}
+
+unsigned long sys_exit(void)
+{
+    return 0;
+}
+
+unsigned long sys_wait(void)
+{
+    return 0;
+}
+
+unsigned long sys_pipe(void)
+{
+    return 0;
+}
+
+unsigned long sys_read(void)
+{
+    return 0;
+}
+
+unsigned long sys_kill(void)
+{
+    return 0;
+}
+
+unsigned long sys_exec(void)
+{
+    return 0;
+}
+
+unsigned long sys_fstat(void)
+{
+    return 0;
+}
+
+unsigned long sys_chdir(void)
+{
+    return 0;
+}
+
+unsigned long sys_dup(void)
+{
+    return 0;
+}
+
+unsigned long sys_getpid(void)
+{
+    return 0;
+}
+
+unsigned long sys_uptime(void)
+{
+    return 0;
+}
+
+unsigned long sys_open(void)
+{
+    return 0;
+}
+
+unsigned long sys_write(void)
+{
+    return 0;
+}
+
+unsigned long sys_mknod(void)
+{
+    return 0;
+}
+
+unsigned long sys_unlink(void)
+{
+    return 0;
+}
+
+unsigned long sys_link(void)
+{
+    return 0;
+}
+
+unsigned long sys_mkdir(void)
+{
+    return 0;
+}
+
+unsigned long sys_close(void)
+{
+    return 0;
+}
+
 void * const sys_call_table[__NR_syscalls] = {
-    sys_printf,
-    sys_sleep,
-    sys_brk,
-    sys_free,
-    sys_ni_syscall
+    sys_printf, //0
+    sys_fork,   //1
+    sys_exit,   //2
+    sys_wait,   //3
+    sys_pipe,   //4
+    sys_read,   //5
+    sys_kill,   //6
+    sys_exec,   //7
+    sys_fstat,  //8
+    sys_chdir,  //9
+    sys_dup,    //10
+    sys_getpid, //11
+    sys_brk,    //12
+    sys_sleep,  //13
+    sys_uptime, //14
+    sys_open,   //15
+    sys_write,  //16
+    sys_mknod,  //17
+    sys_unlink, //18
+    sys_link,   //19
+    sys_mkdir,  //20
+    sys_close,  //21
+    sys_free,   //22
+    sys_ni_syscall, //23
 };

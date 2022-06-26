297a6b757119444e32bb03b86779b8633df8f34d
richard.dai <richarddai@163.com>
2022-06-26 12:12:12 +0000

    add exec empty func and note

--- kernel/exec.c
+++ kernel/exec.c
@@ -77,3 +77,27 @@ bad:
     }
     return -1;
 }
+
+/*
+linux内核中exec函数实现的流程：
+exec系列函数从linux的linux_binfmt链表中，通过依次调用每个结构的load_binary函数来选择合适的运行格式，
+一旦找到就执行load_binary函数，否则尝试下一个linux_binfmt的load_binary，直到尝试完所有的linux_binfmt。
+load_binary函数：
+1.检查128位的magic number，看文件是不是属于这个格式
+2.读取文件的header
+3.从文件得到dynamic linker的位置
+4.检查dynamic linker是否有效
+5.调用flush_old_exec()函数，清除被之前计算所使用的所有资源，像内存，页表
+6.使用do_mmap()将可执行文件的text，data，bss段映射到进程中
+7.如果可执行文件还有其它段，也映射到进程中
+8.加载dynamic linker
+9.由dynamic linker将程序运行所需要的其它库用mmap()映射到进程中
+10.跳到程序的入口出开始执行程序
+
+问题：
+1.在加载新程序的代码段到老进程的代码段后，切换页表后，为什么不会出现问题？
+*/
+int exec(char *path, char *argv)
+{
+    return 0;
+}

f0379fe945a37368d02d1b14f2721aa4124e89d5
richard.dai <richarddai@163.com>
2022-07-14 09:42:46 +0800

    sleep.c: when timeout, timeout_period + HZ

--- kernel/sleep.c
+++ kernel/sleep.c
@@ -10,15 +10,18 @@ void kernel_sleep(uint64 times)
     unsigned long timeout = jiffies + times*HZ;
     set_current_state(TASK_INTERRUPTIBLE);
     wait_event_interruptible(sleep_queue, timer_after_eq(jiffies, timeout));
+
 }
-static unsigned long prev_jiffies = 0;
+static unsigned long timeout_period = 0;
 void wakes_sleep(void)
 {
     /*一秒钟唤醒一次在sleep中睡眠的进程*/
-    if(0 == prev_jiffies){
-        prev_jiffies = jiffies;
+    if(0 == timeout_period){
+        timeout_period = jiffies + HZ;
     } else {
-        if(jiffies >= (prev_jiffies+HZ))
+        if(timer_after_eq(jiffies, timeout_period)){
             wake_up(&sleep_queue);
+            timeout_period = jiffies + HZ;
+        }
     }
 }

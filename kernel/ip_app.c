c253b50c6077bc79f64c84a65ea701ff0dc8d520
richard.dai <richarddai@163.com>
2022-08-04 10:44:26 +0800

    ip_app: add wait arp func, and wake wait_arp thread func

--- kernel/ip_app.c
+++ kernel/ip_app.c
@@ -2,12 +2,93 @@
 #include "net.h"
 #include "types.h"
 #include "debug.h"
+#include "spinlock.h"
+#include "sched.h"
+#include "timer.h"
 
 static ipaddr_t local_ip = MAKE_IP_ADDR(10, 0, 2, 15); // qemu's idea of the guest IP
 static struct eth_addr local_mac = { {0x52, 0x54, 0x00, 0x12, 0x34, 0x56} };
 static struct eth_addr broadcast_mac = {{0xFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF}};
 
 static ipaddr_t broadcast_ipaddr = MAKE_IP_ADDR(0xff,0xff,0xff,0xff);
+
+
+
+arp_waitqueue_t arp_wq_head = {
+    .entry = LIST_HEAD_INIT(arp_wq_head.entry),
+    .p = NULL,
+    .ipaddr = MAKE_IP_ADDR(0, 0, 0, 0),
+    .lock = INIT_SPINLOCK("arp_wq")
+};
+
+int init_arp_waitqueue_entry(arp_waitqueue_t *arpwq, ipaddr_t ipaddr)
+{
+    arpwq->entry.next = arpwq->entry.prev = NULL;
+    initlock(&arpwq->lock, "arp_wq_entry");
+    ipaddr_copy(arpwq->ipaddr, ipaddr);
+    return 0;
+}
+
+int ip_app_add_arp_wq_entry(arp_waitqueue_t *wq_entry)
+{
+    unsigned long flags;
+    if(on_arp_waitqueue(wq_entry)){
+        printk("%s %d: bug\n", __func__, __LINE__);
+        return 0;
+    }
+    spin_lock_irqsave(&arp_wq_head.lock, flags);
+    wq_entry->p = current;
+    list_add_tail(&wq_entry->entry, &arp_wq_head.entry);
+    spin_unlock_irqrestore(&arp_wq_head.lock, flags);
+    return 0;
+}
+
+static int ip_app_del_arp_wq_entry(arp_waitqueue_t *wq_entry)
+{
+    if(!on_arp_waitqueue(wq_entry)){
+        printk("%s %d: bug\n", __func__, __LINE__);
+        return 0;
+    }
+    if(list_empty(&arp_wq_head.entry)){
+        printk("%s %d: ERROR\n", __func__, __LINE__);
+        return -1;
+    }
+
+    list_del(&wq_entry->entry);
+    if(wq_entry->p)
+        wake_up_process(wq_entry->p);
+    return 0;
+}
+
+static int ip_app_arp_wq_try_wakeup_proccess_and_del_entry(ipaddr_t ipaddr)
+{
+    unsigned long flags;
+    int found = 0;
+    arp_waitqueue_t *wq_entry;
+    spin_lock_irqsave(&arp_wq_head.lock, flags);
+    list_for_each_entry(wq_entry, &arp_wq_head.entry, entry){
+       if(ipaddr_cmp(ipaddr, wq_entry->ipaddr)){
+            found = 1;
+            break;
+       }
+    }
+    if(found == 1)
+        ip_app_del_arp_wq_entry(wq_entry);
+    spin_unlock_irqrestore(&arp_wq_head.lock, flags);
+    return 0;
+}
+
+int ip_app_wait_arp_reply(arp_waitqueue_t *wq_entry, ipaddr_t ipaddr, signed timeout)
+{
+    init_arp_waitqueue_entry(wq_entry, ipaddr);
+    ip_app_add_arp_wq_entry(wq_entry);
+    return schedule_timeout(timeout);
+}
+
+int ip_app_wake_arp_process(ipaddr_t ipaddr)
+{
+    return ip_app_arp_wq_try_wakeup_proccess_and_del_entry(ipaddr);
+}
 
 ipaddr_t ip_app_get_broadcast_ipaddr()
 {
--- kernel/ip_app.h
+++ kernel/ip_app.h
@@ -1,6 +1,20 @@
 #ifndef __IP_APP_H__
 #define __IP_APP_H__
 #include "net.h"
+#include "proc.h"
+#include "list.h"
+#include "spinlock.h"
+#include "timer.h"
+
+typedef struct arp_waitqueue{
+    struct task_struct *p;
+    ipaddr_t ipaddr;
+    struct spinlock lock;
+    struct list_head entry;
+    struct timer_list timer;
+}arp_waitqueue_t;
+
+#define on_arp_waitqueue(arpwq) (arpwq->entry.next != NULL)
 
 void print_ipaddr(ipaddr_t *ipaddr);
 ipaddr_t ip_app_get_local_ip(void);
@@ -9,4 +23,12 @@ struct eth_addr *ip_app_get_broadcast_mac(void);
 ipaddr_t ip_app_get_broadcast_ipaddr();
 void print_mac(struct eth_addr *mac);
 
+
+
+/*发出arp后等待arp回应，如果等待超时，则返回0，
+未超时，则返回剩余等待的时间 timeout的单位是一个jiffies*/
+int ip_app_wait_arp_reply(arp_waitqueue_t *wq_entry, ipaddr_t ipaddr, signed timeout);
+
+/*唤醒等待arp响应的进程*/
+int ip_app_wake_arp_process(ipaddr_t ipaddr);
 #endif
